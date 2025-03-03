a3562205f5a13b24c95d43a934597fa0c6b18c3c
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-03 13:47:05 +0800

    init

--- install.sh
+++ install.sh
@@ -11,7 +11,11 @@ echo "重置 easy-sing-box..."
 rm -rf /opt/easy-sing-box-central/
 cd /opt && git clone -q https://github.com/zmlu/easy-sing-box-central.git
 screen -dmS esb-c
-screen -x -S esb-c -p 0 -X stuff "cd /opt/easy-sing-box-central || exit"
-screen -x -S esb-c -p 0 -X stuff "source /opt/venv/easy-sing-box-central/bin/activate"
-screen -x -S esb-c -p 0 -X stuff "pip3 install -r requirements.txt"
-screen -x -S esb-c -p 0 -X stuff "python3 main.py"
+screen -x -S esb-c -p 0 -X stuff "cd /opt/easy-sing-box-central || exit
+"
+screen -x -S esb-c -p 0 -X stuff "source /opt/venv/easy-sing-box-central/bin/activate
+"
+screen -x -S esb-c -p 0 -X stuff "pip3 install -r requirements.txt
+"
+screen -x -S esb-c -p 0 -X stuff "python3 main.py
+"
