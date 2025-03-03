7d62fee46c4f63710e3c52964d2f8830beee75bd
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-03 17:14:04 +0800

    init

--- sb_qrcode.sh
+++ sb_qrcode.sh
@@ -4,8 +4,8 @@ config_file="$HOME/esb-c.config"
 www_dir_random_id=$(jq -r '.www_dir_random_id' "$config_file")
 server_ip=$(jq -r '.server_ip' "$config_file")
 server_port=$(jq -r '.server_port' "$config_file")
-echo "\\e[1;33msing-box\\033[0m"
-echo "\\e[1;32mhttp://${server_ip}:${server_port}/${www_dir_random_id}/sb.json\\033[0m"
-echo ""
+echo -e "\e[1;33msing-box\\033[0m"
+echo -e "\e[1;32mhttp://${server_ip}:${server_port}/${www_dir_random_id}/sb.json\\033[0m"
+echo -e ""
 
 echo "sing-box://import-remote-profile?url=http%3A%2F%2F${server_ip}:${server_port}%2F${www_dir_random_id}%2Fsb.json#esb" | qrencode -o - -t ANSI
