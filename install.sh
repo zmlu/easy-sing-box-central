cda40d26c0a1fd90efcd8c487b70ec003a5186f8
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-03 16:59:04 +0800

    init

--- install.sh
+++ install.sh
@@ -13,11 +13,3 @@ cd /opt && git clone -q https://github.com/zmlu/easy-sing-box-central.git
 screen -dmS esb-c
 screen -x -S esb-c -p 0 -X stuff "cd /opt/easy-sing-box-central && source /opt/venv/easy-sing-box-central/bin/activate && pip3 install -r requirements.txt && echo '啟動服務...' && python3 main.py
 "
-
-config_file="$HOME/esb-c.config"
-www_dir_random_id=$(jq -r '.www_dir_random_id' "$config_file")
-server_ip=$(jq -r '.server_ip' "$config_file")
-server_port=$(jq -r '.server_port' "$config_file")
-echo "\\e[1;33msing-box\\033[0m"
-echo "\\e[1;32mhttp://"server_ip":"+server_port+"/"+www_dir_random_id+"/sb.json\\033[0m"
-echo ""
--- sb_qrcode.sh
+++ sb_qrcode.sh
@@ -5,7 +5,7 @@ www_dir_random_id=$(jq -r '.www_dir_random_id' "$config_file")
 server_ip=$(jq -r '.server_ip' "$config_file")
 server_port=$(jq -r '.server_port' "$config_file")
 echo "\\e[1;33msing-box\\033[0m"
-echo "\\e[1;32mhttp://"server_ip":"+server_port+"/"+www_dir_random_id+"/sb.json\\033[0m"
+echo "\\e[1;32mhttp://$server_ip:$server_port/$www_dir_random_id/sb.json\\033[0m"
 echo ""
 
 echo "sing-box://import-remote-profile?url=http%3A%2F%2F"+server_port+":"+server_port+"%2F8e59d9%2Fsb.json#esb" | qrencode -o - -t ANSI
