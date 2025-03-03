d7ad38f54d4d7fbe958c34cbba513458fc4f801d
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-03 13:42:07 +0800

    init

--- install.sh
+++ install.sh
@@ -10,4 +10,8 @@ cd /opt/venv && python3 -m venv easy-sing-box-central
 echo "重置 easy-sing-box..."
 rm -rf /opt/easy-sing-box-central/
 cd /opt && git clone -q https://github.com/zmlu/easy-sing-box-central.git
-screen -S esb-c -X bash <(curl -Ls https://github.com/zmlu/easy-sing-box-central/raw/master/install_screen.sh?_=$(date +%s))
+screen -dmS esb-c
+screen -x -S esb-c -p 0 -X stuff "cd /opt/easy-sing-box-central || exit"
+screen -x -S esb-c -p 0 -X stuff "source /opt/venv/easy-sing-box-central/bin/activate"
+screen -x -S esb-c -p 0 -X stuff "pip3 install -r requirements.txt"
+screen -x -S esb-c -p 0 -X stuff "python3 main.py"
